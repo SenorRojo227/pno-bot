387b2f1c8e34291ea17a6bd4e69f57014069b6a5
William Galiber <wgaliber@gmail.com>
2024-07-31 15:59:39 -0400

    Debug Bday List

--- src/commands/birthday/viewBdayList.js
+++ src/commands/birthday/viewBdayList.js
@@ -13,16 +13,15 @@ module.exports = {
             const users = await Birthday.find(query).sort('month day');
             const embed = new EmbedBuilder()
                 .setTitle("Birthdays");
-            //const memberList = client.guild.get(interaction.guild.id).members;
+            const memberList = client.guild.get(interaction.guild.id);
             for (const u of users) {
                 try {
-                    let displayName = client.users.cache.get(u.userId).displayName;
+                    let displayName = memberList.members.cache.get(u.userId).displayName;
                     embed.addFields({
                         name: displayName,
                         value: u.month + "/" + u.day + "/" + u.year,
                     });
                 } catch (error) {
-                    console.log(u.userId);
                     embed.addFields({
                         name: "Unknown User",
                         value: u.month + "/" + u.day + "/" + u.year,
