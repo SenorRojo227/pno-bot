fe033fcc64d0d5cf4eaa078d1825c55defab69e9
AryanKumar2002 <akumar54@calpoly.edu>
2024-08-09 13:24:42 -0700

    added removeBday, fixed bug with nextBday
    bug: nextBday causes an error if no birthdays are set in the server

--- src/commands/birthday/nextBday.js
+++ src/commands/birthday/nextBday.js
@@ -13,6 +13,12 @@ module.exports = {
             let index = 0;
             let isBday = false;
             const users = await Birthday.find(query).sort('month day');
+            
+            if (users.length === 0) {
+                interaction.reply("There are no birthdays set in this server.");
+                return;
+            }
+            
             for (let i = 0; i < users.length; i++) {
                 if (users[i].month > date.getMonth() + 1 || (users[i].month == date.getMonth() + 1 && users[i].day > date.getDate())) {
                     index = i;
@@ -20,10 +26,16 @@ module.exports = {
                 } else if (users[i].month == date.getMonth() + 1 && users[i].day == date.getDate()) {
                     isBday = true;
                 }
-            }   
-            const displayName = await interaction.guild.members.cache.get(users[index].userId).displayName;
+            }
+            
+            const displayName = await interaction.guild.members.cache.get(users[index].userId)?.displayName;
+            if (!displayName) {
+                interaction.reply("There are no upcoming birthdays set in this server.");
+                return;
+            }
+
             if (isBday) {
-                const bdayName = await interaction.guild.members.cache.get(users[index-1].userId).displayName;
+                const bdayName = await interaction.guild.members.cache.get(users[index - 1].userId)?.displayName;
                 interaction.reply("Today is " + bdayName + "'s birthday! The next birthday is " + displayName + "'s on "  + users[index].month + "/" + users[index].day + "!");
             } else {
                 interaction.reply("The next birthday is " + displayName + "'s on "  + users[index].month + "/" + users[index].day + "/" + users[index].year + "!");
@@ -31,6 +43,7 @@ module.exports = {
             
         } catch (error) {
             console.log("Error viewing: " + error);
+            interaction.reply("An error occurred while trying to retrieve the next birthday.");
         }
     }
 }
